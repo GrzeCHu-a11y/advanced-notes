e295412d39e0304cc43fa102e7aa9553c1eb26b8
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-04-15 20:11:09 +0200

    added basic home page design and improved menu

--- layout.php
+++ layout.php
@@ -12,26 +12,26 @@
 
     <!-- navbar -->
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">NoteSphere</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">NoteSphere</a>
             <div class="collapse navbar-collapse" id="navbarExample">
-                <ul class="navbar-nav me-auto mb-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">About Project</a>
+                        <a class="nav-link" href="#">About Project</a>
                     </li>
                 </ul>
-                <div class="d-flex align-items-center flex-column flex-lg-row">
-                    <button type="button" class="btn btn-default px-3 me-2">
+                <div class="d-flex align-items-center">
+                    <button type="button" class="btn btn-outline-secondary me-2">
                         Login
                     </button>
-                    <button type="button" class="btn btn-primary me-3">
+                    <button type="button" class="btn btn-primary">
                         Sign up for free
                     </button>
                 </div>
@@ -39,7 +39,12 @@
         </div>
     </nav>
 
+
     <!-- navbar -->
+
+    <main>
+        <?php require_once("pages/home.php") ?>
+    </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
---
+++ pages/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NoteSphere</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to NoteSphere!</h1>
+        <p>Create, manage, and organize your notes in a simple way.</p>
+        <a href="#about" class="btn">Learn More</a>
+    </header>
+
+    <section id="about">
+        <div class="container">
+            <h2>About the App</h2>
+            <br>
+            <p>Our note-taking app has been designed with simplicity and usability in mind. Whether you need to quickly jot down important information or create detailed action plans, our app has everything you need.</p>
+            <p>We provide:</p>
+            <ul>
+                <li>Fast creation of new notes</li>
+                <li>Easy management of notes</li>
+                <li>Mobility - access to notes from any device</li>
+                <li>Security of your data</li>
+            </ul>
+            <br>
+            <p>If you want to test the app without creating an account:</p>
+            <ul>
+                <li>Go to the login tab</li>
+                <li>In the login form, enter the username: user</li>
+                <li>In the login form, enter the password: user</li>
+            </ul>
+            <br>
+            <a href="#signup" class="btn">Sign Up Now!</a>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2024 My Notes App. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
