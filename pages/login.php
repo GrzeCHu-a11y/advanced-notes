467c4a7a0219746013055642a133e3ba00ecca5e
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-04-17 16:53:48 +0200

    added login and register pages

--- pages/login.php
+++ pages/login.php
@@ -15,12 +15,12 @@
                 <h2>Login</h2>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" />
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password" />
                 </div>
                 <div class="text-end">
                     <button type="submit" class="btn btn-subtle me-2">Cancel</button>
@@ -29,7 +29,6 @@
             </form>
         </div>
     </section>
-
 </body>
 
 </html>
--- pages/register.php
+++ pages/register.php
@@ -4,11 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>register</h1>
+    <section>
+        <div class="container">
+            <form method="post">
+                <h2>Register</h2>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" />
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password" />
+                </div>
+                <div class="text-end">
+                    <button type="submit" class="btn btn-subtle me-2">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
