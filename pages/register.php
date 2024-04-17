914dabc8c0d242b7e07574a786cc242befcaa82b
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-04-17 18:50:39 +0200

    added user registration system

--- pages/register.php
+++ pages/register.php
@@ -1,12 +1,17 @@
 <?php
 
 declare(strict_types=1);
-
 include_once("./src/RegisterController.php");
-$registerController = new RegisterController;
+
+$registerController = new RegisterController();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $registerController->handleRegister();
+
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $registerController->handleRegister($email, $password, $username);
 }
 
 ?>
@@ -30,6 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" />
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputUsername" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="username" name="username" />
+                    <div id="username" class="form-text">username</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
--- src/RegisterController.php
+++ src/RegisterController.php
@@ -1,11 +1,33 @@
 <?php
 
 declare(strict_types=1);
+require_once("database_config/config.php");
 
 class RegisterController
 {
-    public function handleRegister()
+    private $dbConfig;
+
+    public function __construct()
     {
-        echo $_POST["email"];
+        $this->dbConfig = new Config();
+    }
+
+    public function handleRegister(string $email, string $password, string $username)
+    {
+        $conn = new mysqli(Config::$servername, Config::$username, Config::$password, Config::$database);
+
+        if ($conn->connect_error) {
+            die("Connection Failed:" . $conn->connect_error);
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $sql = $conn->prepare("INSERT INTO users (email, password, username) VALUES (?, ?, ?)");
+        $sql->bind_param("sss", $email, $hashedPassword, $username);
+
+        if ($sql->execute()) {
+            echo "User Created";
+        } else echo "error" . $sql->error;
+
+        $conn->close();
     }
 }
