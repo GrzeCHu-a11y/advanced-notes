122ef1015d27ea1116c521ee1f24227e2a0707f9
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-04-24 19:27:15 +0200

    added display notes system in dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,8 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 if (!isset($_SESSION["id"])) {
     header("Location: /");
 }
+
+require_once("helpers/fetchAllNotes.php");
+
+$fetchAllNotes = new FetchAllNotes();
+$notes = $fetchAllNotes->getAllNotes();
 
 ?>
 <!DOCTYPE html>
@@ -17,9 +24,24 @@ if (!isset($_SESSION["id"])) {
 <body>
     <section>
         <div class="container">
-            <h3><?php echo "Welcome" . " " . $_SESSION["username"] ?></h3>
-            <h3>Recent</h3>
+            <div class="accordion" id="accordionExample">
+                <?php foreach ($notes as $key => $note) : ?>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="heading<?php echo $key; ?>">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $key; ?>" aria-expanded="false" aria-controls="collapse<?php echo $key; ?>">
+                                <?php echo $note['title'] ?>
+                            </button>
+                        </h2>
+                        <div id="collapse<?php echo $key; ?>" class="accordion-collapse collapse" aria-labelledby="heading<?php echo $key; ?>" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <?php echo $note['content'] ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
+        <br>
     </section>
 </body>
 
